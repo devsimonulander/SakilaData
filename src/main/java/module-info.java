e0f039e033e36ac4d5a3598c14e1090118537802
Simon Ulander <devsimonulander@gmail.com>
2023-03-31 10:46:37 +0200

    Fixade dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.sakiladata {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
 
 
     opens se.yalar.grupp5.sakiladata to javafx.fxml;
