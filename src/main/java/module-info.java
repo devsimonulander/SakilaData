c24e5388b54a221b7bae3f91547dfd057f512dff
Simon Ulander <devsimonulander@gmail.com>
2023-04-07 13:29:19 +0200

    Byggde upp de två resterande handlers som jag skulle fixa och förenklade interfacet så att det säger t.ex. insert istället för insertTabell.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sakiladata {
     requires javafx.fxml;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
+    requires java.persistence;
 
 
     opens se.yalar.grupp5.sakiladata to javafx.fxml;
