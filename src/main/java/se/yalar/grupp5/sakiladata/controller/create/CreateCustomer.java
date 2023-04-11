ad756b410a78fc12986a0cd4bdf7293463bee3cf
Simon Ulander <devsimonulander@gmail.com>
2023-04-11 22:33:30 +0200

    Skapade createcustomer men den behöver fixas pga fel i GUI designen.

--- src/main/java/se/yalar/grupp5/sakiladata/controller/create/CreateCustomer.java
+++ src/main/java/se/yalar/grupp5/sakiladata/controller/create/CreateCustomer.java
@@ -3,8 +3,29 @@ package se.yalar.grupp5.sakiladata.controller.create;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import se.yalar.grupp5.sakiladata.Handlers.AddressHandler;
+import se.yalar.grupp5.sakiladata.Handlers.CustomerHandler;
+import se.yalar.grupp5.sakiladata.entities.Address;
+import se.yalar.grupp5.sakiladata.entities.Customer;
 
 public class CreateCustomer {
     @FXML
     private TextField tfFirstName, tfLastName, tfEmail, tfAktiv, tfCustomerId, tfStoreId, tfAddressId;
+
+    void create() {
+        Customer customer = new Customer();
+
+        customer.setFirstName(tfFirstName.getText());
+        customer.setLastName(tfLastName.getText());
+        customer.setEmail(tfEmail.getText());
+        customer.setActive(Boolean.parseBoolean(tfAktiv.getText())); //fel typ på fält
+        //customer.setStores(); //wtf
+
+        AddressHandler addressHandler = new AddressHandler();
+        Address address = addressHandler.getById(Integer.parseInt(tfAddressId.getText()));
+        customer.setAddress(address);
+
+        CustomerHandler customerHandler = new CustomerHandler();
+        customerHandler.insert(customer);
+    }
 }
