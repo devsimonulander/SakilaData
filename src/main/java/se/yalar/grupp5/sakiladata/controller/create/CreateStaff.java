dcdaf915d62b210a85147e7908a6201817c1b8e2
Simon Ulander <devsimonulander@gmail.com>
2023-04-12 00:59:59 +0200

    Skrev CreateStaff.

--- src/main/java/se/yalar/grupp5/sakiladata/controller/create/CreateStaff.java
+++ src/main/java/se/yalar/grupp5/sakiladata/controller/create/CreateStaff.java
@@ -3,8 +3,35 @@ package se.yalar.grupp5.sakiladata.controller.create;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import se.yalar.grupp5.sakiladata.Handlers.AddressHandler;
+import se.yalar.grupp5.sakiladata.Handlers.StoreHandler;
+import se.yalar.grupp5.sakiladata.entities.Address;
+import se.yalar.grupp5.sakiladata.entities.Staff;
+import se.yalar.grupp5.sakiladata.entities.Store;
 
 public class CreateStaff {
     @FXML
     private TextField tfStaffId, tfFirstName, tfLastName, tfAddressId, tfEmail, tfStoreId, tfActive, tfUsername, tfPassword;
+
+    void create() {
+        Staff staff = new Staff();
+
+        staff.setFirstName(tfFirstName.getText());
+        //staff.setLastName(tfLastName.getText()); <-- Fel datatyp
+
+        AddressHandler addressHandler = new AddressHandler();
+        Address address = addressHandler.getById(Integer.parseInt(tfAddressId.getText()));
+        staff.setAddress(address);
+
+        staff.setEmail(tfEmail.getText());
+
+        StoreHandler storeHandler = new StoreHandler();
+        Store store = storeHandler.getStoreByID(Integer.parseInt(tfStoreId.getText()));
+        staff.setStore(store);
+
+
+        staff.setActive(Boolean.parseBoolean(tfActive.getText()));
+        staff.setUserName(tfUsername.getText());
+        staff.setPassword(tfPassword.getText());
+    }
 }
